a4d1ec1484cbba1fba701eb685282901a88f4330
ilya-sigma-ui <slight.loon.alua@mask.me>
2025-10-26 10:38:53 +0300

    1

--- flask-server.py
+++ flask-server.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/catalog")
+def catalog():
+    return render_template('catalog/index.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
